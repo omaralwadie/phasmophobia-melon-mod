becf23a91ad11af82189b0758cd65332155af678
Cr4nkSt4r <dominicspoerhase@aol.com>
2021-02-18 16:45:11 +0100

    Initial Upload: v0.7.5.2a

--- Properties/BuildInfo.cs
+++ Properties/BuildInfo.cs
@@ -6,7 +6,7 @@
         public const string Description = "Cr4nkSt4rs Phasmophobia MelonLoader Mod";
         public const string Author = "Cr4nkSt4r";
         public const string Company = null;
-        public const string Version = "0.7.5.2";
+        public const string Version = "0.7.5.2a";
         public const string DownloadLink = "https://www.unknowncheats.me/forum/phasmophobia/424588-melonloader-esp-basic-informations-active-missions-sourcecode.html";
         public const string GameName = "Phamophobia";
         public const string GameDev = "Kinect Games";
